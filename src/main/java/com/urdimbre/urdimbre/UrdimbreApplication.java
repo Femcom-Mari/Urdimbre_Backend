7d3a4bae501bd825718fe6c777f1ac9850fee893
Femcom-Mari <bongollmaria@gmail.com>
2025-05-28 15:37:47 +0200

    Add existing project files to Git

---
+++ src/main/java/com/urdimbre/urdimbre/UrdimbreApplication.java
@@ -0,0 +1,13 @@
+package com.urdimbre.urdimbre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdimbreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrdimbreApplication.class, args);
+	}
+
+}
